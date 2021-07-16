ac7f64a67f5577bc945e3c5b6b9fa56f028b281a
Adarsh Sethi <66685845+adarsh2104@users.noreply.github.com>
2021-07-16 16:21:49 +0530

    Update settings.py

--- MainSite/settings.py
+++ MainSite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = ['*']
-#ALLOWED_HOSTS = ['italian-delicaciess.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['italian-delicaciess.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
